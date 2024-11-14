96863b5caf2fdf632ca82c3de9256e78a042b7c4
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-11-14 12:44:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "16457832"
+# -------------------------------------------------------------
+API_HASH = "3030874d0befdb5d05597deacc3e83ab"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAHONy8JYrwzoCGNCzVcnADsjeRHjVOjbwY")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
+SUPPORT_GRP = "+OL6jdTL7JAJjYzVl"
+UPDATE_CHNL = "BABY09_WORLD"
+OWNER_USERNAME = "UTTAM470"
