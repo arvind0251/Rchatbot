3a64f667d0c15f6ab4c5db61a051b29613a0b9da
Humbler9931 <163394065+Humbler9931@users.noreply.github.com>
2024-12-02 22:44:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "16457832"
+API_ID = "27015202"
 # -------------------------------------------------------------
-API_HASH = "3030874d0befdb5d05597deacc3e83ab"
+API_HASH = "b817ca2d21c5471522ec93b819301d56"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAHONy8JYrwzoCGNCzVcnADsjeRHjVOjbwY")
+BOT_TOKEN = getenv("BOT_TOKEN", "6897733210:AAF7GKF14VbqQRWevtz8WqG7IR1-hnKd18g")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
-SUPPORT_GRP = "+OL6jdTL7JAJjYzVl"
-UPDATE_CHNL = "BABY09_WORLD"
-OWNER_USERNAME = "UTTAM470"
+OWNER_ID = int(getenv("OWNER_ID", "5789538424"))
+SUPPORT_GRP = "+ewxJNislsIdkMzVl"
+UPDATE_CHNL = "bestshayri_raj"
+OWNER_USERNAME = "promotionyoutubr"
